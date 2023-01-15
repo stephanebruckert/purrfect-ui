ffa05a7ee9b8bb3ab8f63c576e7f72d9bdbe7a65
Stephane Bruckert <stephane.bruckert@gmail.com>
2023-01-15 15:01:53 +0000

    Display API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({
+    'total_cancelled': '',
+    'total_orders': ''
+  });
+
+  const fetchData = () => {
+    return fetch("http://localhost:3000/totals")
+        .then((response) => response.json())
+        .then((data) => setUser(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>Total cancelled: {user['total_cancelled']}</li>
+          <li>Total orders: {user['total_orders']}</li>
+        </ul>
       </header>
     </div>
   );
